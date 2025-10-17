059aafa77cb1a21ebf309a1181c9d8f158dd1340
Sirkkouga <javiabellanieto2001@gmail.com>
2025-10-18 00:48:17 +0200

    Player right left

--- Falles-gaming/Assets/Scripts/Player/PlayerMovement.cs
+++ Falles-gaming/Assets/Scripts/Player/PlayerMovement.cs
@@ -2,6 +2,16 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
+    bool isGrounded = true;
+    public float jumpForce = 10.0f;
+    public float moveSpeed = 5.0f;
+    public float gravity = 9.0f;
+    public float groundCheckDistance = 0.1f;
+
+    public LayerMask groundLayer;
+    private Rigidbody rb;
+
+
     void Start()
     {
         Debug.Log("PlayerMovement script has started.");
@@ -9,10 +19,32 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space))
+
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
-            Debug.Log("Space key was pressed.");
-            PlayerJump();
+            MovePlayer(Vector3.left * moveSpeed);
+            Debug.Log("Player moved left.");
+        }
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            MovePlayer(Vector3.right * moveSpeed);
+            Debug.Log("Player moved right.");
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            if (isGrounded)
+            {
+                PlayerJump();
+                isGrounded = false;
+                Debug.Log("Player is jumping.");
+            }
+            else {
+                if (Physics.Raycast(transform.position, Vector3.down, groundCheckDistance, groundLayer))
+                {
+                    isGrounded = true;
+                    Debug.Log("Player has landed and is grounded.");
+                }
+            }
         }
         // Player movement logic will go here
     }
@@ -25,6 +57,7 @@ public class PlayerMovement : MonoBehaviour
     void PlayerJump()
     {
         // Jump logic will go here
+        transform.position += Vector3.up * jumpForce * Time.deltaTime;
         Debug.Log("Player jumped!");
     }
 
